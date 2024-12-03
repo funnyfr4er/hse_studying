fc9f0d9470e6a7eb49b78b7e4300c03acc4b5e8d
Leonid <leopanch10@gmail.com>
2024-12-04 02:24:56 +0300

    Update script.js

--- html_css_hw_2/script.js
+++ html_css_hw_2/script.js
@@ -5,12 +5,12 @@ const coffees = [
       price: 1500
     },
     {
-      image: "black-iwory.JPG",
+      image: "black-iwory.jpg",
       description: "black iwory. слоновый.",
       price: 40000
     },
     {
-      image: "blue-mountain.JPG",
+      image: "blue-mountain.jpg",
       description: "blue mountatin. ямайка.",
       price: 20000
     },
@@ -20,27 +20,27 @@ const coffees = [
       price: 33000
     },
     {
-      image: "luwak.JPG",
+      image: "luwak.jpg",
       description: "luwak. горькая бразилия.",
       price: 12000
     },
     {
-      image: "mogiana.JPG",
+      image: "mogiana.jpg",
       description: "mogiana. терпкая бразилия.",
       price: 14000
     },
     {
-        image: "robusta.JPG",
+        image: "robusta.jpg",
         description: "robusta. зарядись.",
         price: 2500
       },
       {
-        image: "jacu-bird.JPG",
+        image: "jacu-bird.jpg",
         description: "jacu bird. птичий вкус.",
         price: 90000
       },
       {
-        image: "geisha-la-esmeralda.JPG",
+        image: "geisha-la-esmeralda.jpg",
         description: "la esmeralda. панама.",
         price: 3000
       }
